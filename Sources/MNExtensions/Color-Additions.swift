ef3a1733fb674ae6aede20fa7b3819ea49f7ce2d
Tami Black <missingnoodle@me.com>
2021-01-31 10:09:08 -0800

    functionality: Now implementing the home screen. So far our home view has simply been a host for adding test data, but that changes now: we’re going to make the home view a summary of all their project progress, plus the most important items coming up next. In our three previous tabs we’ve shown off lists and grids, building a perfectly serviceable app for iOS. For this Home tab I want to steer us in a slightly different direction by building a custom layout – nothing dramatic, because after all the goal of this course is to demonstrate best practices and system integrations rather than fancy UI designs, but hopefully it gives you enough scope to customize this view however you want to show off your SwiftUI skills.
    But first, we need to do something simple and important: we need to define some new colors for this screen. You see, up to now we’ve been using either the built-in semantic colors like primary and secondary, or using colors I provided through the asset catalog. Both of these are great choices for different reasons, but this screen needs something that SwiftUI can’t currently provide: the full range of semantic system colors.
    
    You see, at the same time as Apple introduced SwiftUI at WWDC19, they also introduced dark mode for UIKit that allowed us to use semantic colors for the first time. Sadly, for whatever reason Apple only provided a subset of semantic colors inside SwiftUI, which means for example we don’t get background colors and more.
    
    Now, obviously we could define these colors in an asset catalog by hand, but where possible it’s preferable to use the built-in system colors. Not only does that ensure our colors behave correctly right now, but it also ensures our code updates correctly as Apple makes changes in the future.

---
+++ Sources/MNExtensions/Color-Additions.swift
@@ -0,0 +1,14 @@
+//
+//  Color-Additions.swift
+//  
+//
+//  Created by Tami Black on 1/31/21.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
